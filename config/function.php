b808ce0add6e130ba1317572d05c35941336a62c
Gabriel Otemu <otemugabriel@gmail.com>
2026-07-04 11:42:11 +0100

    new

--- config/function.php
+++ config/function.php
@@ -38,7 +38,7 @@ function sendSupportMail($email, $subject, $message, $replyTo) {
         $resend = \Resend::client($key);
 
         $resend->emails->send([
-            'from'     => 'Support <support@hostheritage.com>', 
+            'from' => 'Support  <onboarding@mail.hostheritage.com>', 
             'to'       => [$email],
             'subject'  => $subject,
             'html'     => $message,
